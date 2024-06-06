2b7639420b013183ab0175ab1f3dfa31866db6bb
edwardlosangeles <edward.n.delacruz@gmail.com>
2024-06-06 16:42:45 -0700

    added markazi text and changed className for footerContentContainer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <footer>
-      <section className='contentContainer footerContent'>
+      <section className='footerContentContainer footerContent'>
         <div> <Link to="/"><img className='footerImg' src={logo} alt="header logo" /></Link> </div>
         <div className="footerContentSansLogo">
           <div className='flexChild'>
-            <h4 style={{color: 'var(--primaryDark)'}}>Site Map</h4><br />
+            <h4 style={{ color: 'var(--primaryDark)' }}>Site Map</h4><br />
             <ul className='footerSiteMapUL'>
               <li><Link className='link' to="/">Home</Link></li>
               <li>About</li>
@@ -19,13 +19,13 @@ export default function Footer() {
               <li>Order Online</li>
               <li>Login</li> </ul> </div>
           <div className='flexChild'>
-            <h4 style={{color: 'var(--primaryDark)'}}>Contact</h4><br />
+            <h4 style={{ color: 'var(--primaryDark)' }}>Contact</h4><br />
             123 Somestreet Blvd.<br />
             Chicago, IL 60638<br />
             (123) 456-7890<br />
             mail@example.com </div>
           <div className='flexChild'>
-            <h4 style={{color: 'var(--primaryDark)'}}>Social Media Links</h4><br />
+            <h4 style={{ color: 'var(--primaryDark)' }}>Social Media Links</h4><br />
             <ul className='footerSiteMapUL'>
               <li>Facebook</li>
               <li>Instagram</li>
