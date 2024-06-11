28bf61c357c477e3523f8c243356d464c9a24171
edwardlosangeles <edward.n.delacruz@gmail.com>
2024-06-10 17:27:30 -0700

    broke up sections in HomePage.js & BookingPage.js into separate components

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,13 +1,15 @@
 // src/pages/BookingPage.js
 // ====================================================
 import React from 'react'
-import { Link } from 'react-router-dom'
-export default function BookingPage() {
-  return ( <>
-      <section className='bookingHero'>
-        <div className='contentArea'>
-          <div><h1>Reserve a Table</h1>
-            <p>Easily and conveniently reserve a table for your party online! Simply fill
-              out the form below and you will receive a confirmation immediately.</p> </div>
-          <img src={require('../assets/stock_restaurant.jpg')} alt="hero reserve" /> </div> </section>
-      <section>Reservation Form Section</section> </>);}
+// import { Link } from 'react-router-dom'
+const BookingHero = () =>
+  <section className='bookingHero'>
+    <div className='contentArea'>
+      <div><h1>Reserve a Table</h1>
+        <p>Easily and conveniently reserve a table for your party online! Simply fill
+          out the form below and you will receive a confirmation immediately.</p> </div>
+      <img src={require('../assets/stock_restaurant.jpg')} alt="hero reserve" /> </div> </section>;
+const ReserveForm = () => <section>Reservation Form Section</section>;
+  <section>Reservation Form Section</section>
+const BookingPage = () => <> <BookingHero /> <ReserveForm /> </>;
+export default BookingPage;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -36,47 +36,51 @@ const reviewCardList = [ /* data for building ReviewCard component to display th
     reviewPic: () => require('../assets/review_Zachary G.jpeg'),
     reviewName: 'Zachary G.',
     reviewMessage: 'Long wait, but totally worth it. Lots of healthy choices!', }, ];
-export default function HomePage() {
-  return ( <>
-      <section className='homeHero'>
-        <div className='contentArea'>
-          <div><h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-            <Link className='link' to="/reserve"><button>Reserve a Table</button></Link> </div>
-          <img src={require('../assets/hero_waiter_tray.jpg')} alt="hero home" /> </div> </section>
-      <section className='homeSpecials'>
-        <div className='contentArea'>
-          <h1>This week's specials!</h1>
-          <div><button>Online Menu</button></div> {/* wrapping button in div retains its size. doesn't stretch to grid cell boundary */}
-          <div className='dishCards'>
-          {dishCardList.map((dishCard) => ( /* map through dishCardList and generate a card for each dish using DishCard component */
-            <DishCard
-              key={dishCard.dishName}
-              dishImage={dishCard.dishImage()}
-              dishName={dishCard.dishName}
-              price={dishCard.price}
-              description={dishCard.description} />))} </div> </div> </section>
-      <section className='homeTestimonials'>
-        <div className='contentArea'>
-          <h1>Testimonials</h1>
-          <div className='reviews'>
-            {reviewCardList.map((reviewCard) => (
-              <ReviewCard
-              key={reviewCard.reviewName}
-              reviewRating={reviewCard.reviewRating}
-              reviewPic={reviewCard.reviewPic()}
-              reviewName={reviewCard.reviewName}
-              reviewMessage={reviewCard.reviewMessage} /> )) } </div> </div> </section>
-      <section className='homeDescription'>
-        <div className='contentArea'>
-          <div className="descriptionText"><h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>Little Lemon is owned by two Italian brothers, Mario and Adrian, who moved to the United States to pursue their shared
-              dream of owning a restaurant. To craft the menu, Mario relies on family recipes and his experience as a chef in Italy.
-              Adrian does all the marketing for the restaurant and led the effort to expand the menu beyond classic Italian to
-              incorporate additional cuisines from the Mediterranean region.</p> </div>
-          <div className="descriptionImages">
-            <img className='img-1' src={require('../assets/owners_1.jpg')} alt="owner Mario" />
-            <img className='img-2'src={require('../assets/owners_2.jpg')} alt="owner Adrian" />
-          </div> </div> </section> </>); }
+const CallToAction = () =>
+  <section className='homeHero'>
+    <div className='contentArea'>
+      <div><h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        <Link className='link' to="/reserve"><button>Reserve a Table</button></Link> </div>
+      <img src={require('../assets/hero_waiter_tray.jpg')} alt="hero home" /> </div> </section>;
+const Specials = () =>
+  <section className='homeSpecials'>
+    <div className='contentArea'>
+      <h1>This week's specials!</h1>
+      <div><button>Online Menu</button></div> {/* wrapping button in div retains its size. doesn't stretch to grid cell boundary */}
+      <div className='dishCards'>
+      {dishCardList.map((dishCard) => ( /* map through dishCardList and generate a card for each dish using DishCard component */
+        <DishCard
+          key={dishCard.dishName}
+          dishImage={dishCard.dishImage()}
+          dishName={dishCard.dishName}
+          price={dishCard.price}
+          description={dishCard.description} />))} </div> </div> </section>;
+const CustomersSay = () =>
+  <section className='homeTestimonials'>
+    <div className='contentArea'>
+      <h1>Testimonials</h1>
+      <div className='reviews'>
+        {reviewCardList.map((reviewCard) => (
+          <ReviewCard
+          key={reviewCard.reviewName}
+          reviewRating={reviewCard.reviewRating}
+          reviewPic={reviewCard.reviewPic()}
+          reviewName={reviewCard.reviewName}
+          reviewMessage={reviewCard.reviewMessage} /> )) } </div> </div> </section>;
+const Chicago = () =>
+  <section className='homeDescription'>
+    <div className='contentArea'>
+      <div className="descriptionText"><h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Little Lemon is owned by two Italian brothers, Mario and Adrian, who moved to the United States to pursue their shared
+          dream of owning a restaurant. To craft the menu, Mario relies on family recipes and his experience as a chef in Italy.
+          Adrian does all the marketing for the restaurant and led the effort to expand the menu beyond classic Italian to
+          incorporate additional cuisines from the Mediterranean region.</p> </div>
+      <div className="descriptionImages">
+        <img className='img-1' src={require('../assets/owners_1.jpg')} alt="owner Mario" />
+        <img className='img-2'src={require('../assets/owners_2.jpg')} alt="owner Adrian" />
+      </div> </div> </section>;
+const HomePage = () => <> <CallToAction /> <Specials /> <CustomersSay /> <Chicago /> </>;
+export default HomePage;
