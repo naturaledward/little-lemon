c5c7a46b5809ad221517de86cff0d88fba2eb475
edwardlosangeles <edward.n.delacruz@gmail.com>
2024-06-12 16:49:12 -0700

    slight changes to BookingPage.js forgotten from last commit

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -9,7 +9,7 @@ const BookingHero = () =>
         <p>Easily and conveniently reserve a table for your party online! Simply fill
           out the form below and you will receive a confirmation immediately.</p> </div>
       <img src={require('../assets/stock_restaurant.jpg')} alt="hero reserve" /> </div> </section>;
-const ReserveForm = () => <section>Reservation Form Section</section>;
-  <section>Reservation Form Section</section>
-const BookingPage = () => <> <BookingHero /> <ReserveForm /> </>;
+const BookingForm = () =>
+  <section className='bookingForm'>Reservation Form Section</section>
+const BookingPage = () => <> <BookingHero /> <BookingForm /> </>;
 export default BookingPage;
