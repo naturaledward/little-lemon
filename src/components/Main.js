21e790cad0fa45e10d3f467f89564c83fb863f89
edwardlosangeles <edward.n.delacruz@gmail.com>
2024-06-18 14:35:11 -0700

    lifted state of available time slots and changed it to a reducer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,45 @@
 // src/components/Main.js
 // ====================================================
-import React from 'react'
+import { useReducer } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import BookingPage from '../pages/BookingPage'
 export default function Main() {
+  // time slots to initialize the availableTimes state
+  const initializeSlots = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
+  /* The page should display the existing booked table times & available slots
+  using a list component containing several instances of a BookingSlot component.
+  Available booking slots will be shared between the components and updated when the user submits the form */
+  // an <option> for a <select> form input field. Whatever is passed to this component will be the contents of the option
+  const BookingSlot = ({ children }) => <option>{children}</option>
+  // map through array passed to it and generate a list of BookingSlot components with the array item as its child
+  const AvailableSlots = times => times.map(i => <BookingSlot key={i}>{i}</BookingSlot>)
+  // initialize availableTimes state as a list of time slots from initializeSlots
+  const initializeTimes = AvailableSlots(initializeSlots)
+  // reducer function for availableTimes state
+  // certain available times under time input field will be displayed depending on what date is used in the form
+  const updateTimes = (v, { type }) => {
+    // make it so time options leaves out 17:00 if today's date is selected and both 17:00 & 18:00 if tomorrow's date is selected
+    const today = new Date() //get today's date & convert it to <input type='date'> input date format: yyyy-mm-dd
+    const todayFormatted = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0')
+      + '-' + String(today.getDate()).padStart(2, '0')
+    // get a formated version of tomorrow's date
+    const tomorrowFormatted = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0')
+      + '-' + String(today.getDate() + 1).padStart(2, '0')
+    if (type === todayFormatted)
+      return (['18:00', '19:00', '20:00', '21:00', '22:00'].map(i => <BookingSlot key={i}>{i}</BookingSlot>))
+    if (type === tomorrowFormatted)
+      return (['19:00', '20:00', '21:00', '22:00'].map(i => <BookingSlot key={i}>{i}</BookingSlot>))
+    // if none of the actions selected, then don't update the state, hence what is returned for update is just the current state
+    return v }
+  // state lifted from BookingPage.js to be passed to BookingPage.js instead along with its dispatch function, state changed to a reducer
+  const [availableTimes, dispatchTimes] = useReducer(updateTimes, initializeTimes)
   return (
     <main className='page'>
       <Routes>
         {/* default route if nothing typed in browser address */}
         <Route path='/' element={<HomePage />} />
         {/* append /reserve in browser address to go here */}
-        <Route path='/reserve' element={<BookingPage />} />
+        <Route path='/reserve' element={<BookingPage v={availableTimes} f={dispatchTimes} />} />
       </Routes>
-    </main>); }
+    </main>) }
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -8,11 +8,16 @@ const BookingHero = () =>
         <p>Easily and conveniently reserve a table for your party online! Simply fill
           out the form below and you will receive a confirmation immediately.</p> </div>
       <img src={require('../assets/stock_restaurant.jpg')} alt="hero reserve" /> </div> </section>;
-const BookingForm = () => {
+// availableTimes is a list of available times to make a reservation for, displayed as a list of options for <select> input on the form
+const BookingForm = ({ availableTimes, dispatchTimes }) => {
   const handleChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
-  const formDataInitialObj = { dateVal: '', timeVal: '17:00', guests: '1', occasion: 'Birthday' }
-  const [availableTimes] = useState(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'])
-  const listTimes = availableTimes.map(i => <option key={i}>{i}</option>)
+  /* in addition to updating date state value like in handleChange, call the dispatch function of availableTimes state
+  and pass to it the date that was selected for the date form field
+  dispatch function will update availableTimes based on that date to be displayed as a list of options for the time form input field */
+  const handleChangeResDate = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+    dispatchTimes({ type: e.target.value }) }
+  const formDataInitialObj = { dateVal: '', timeVal: '', guests: '1', occasion: 'Birthday' }
   const [formData, setFormData] = useState(formDataInitialObj)
   const handleSubmit = e => {
     e.preventDefault(); /* prevent default behavior upon submit  */
@@ -22,10 +27,12 @@ const BookingForm = () => {
   return <section className='bookingForm'> <div className='contentArea'>
     <form onSubmit={handleSubmit} className='formElement'>
       <label htmlFor='res-date'>Choose date</label>
-      <input id='res-date' type='date' name='dateVal' value={formData.dateVal} onChange={handleChange} />
+      {/* on change of the date field, the dispatch for availableTimes state is called to update time field options */}
+      <input id='res-date' type='date' name='dateVal' value={formData.dateVal} onChange={handleChangeResDate} />
       <label htmlFor='res-time'>Choose time</label>
       <select id='res-time' name='timeVal' value={formData.timeVal} onChange={handleChange}>
-        {listTimes}</select>
+        {/* a list of <option> tags passed from Main.js that contain all available time slots for a particular date input */}
+        {availableTimes}</select>
       <label htmlFor='guests'>Number of guests</label>
       <input id='guests' type='number' placeholder='1' min='1' max='10' name='guests' value={formData.guests} onChange={handleChange} />
       <label htmlFor="occasion">Occasion</label>
@@ -33,5 +40,5 @@ const BookingForm = () => {
         <option>Birthday</option>
         <option>Anniversary</option> </select>
       <button type="submit">Make Your reservation</button> </form> </div> </section> }
-const BookingPage = () => <> <BookingHero /> <BookingForm /> </>;
+const BookingPage = ({ v, f }) => <> <BookingHero /> <BookingForm availableTimes={v} dispatchTimes={f} /> </>;
 export default BookingPage;
