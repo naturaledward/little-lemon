58fffd7f732f660a964cd36fa5b5247e1b33c310
edwardlosangeles <edward.n.delacruz@gmail.com>
2024-06-20 13:14:55 -0700

    minor changes to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,8 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import BookingPage from '../pages/BookingPage'
 export default function Main() {
+  // temporary array for holding mock available times if today or tomorrow is seleted
+  const dummyTimes = ['17:00','18:00', '19:00', '20:00', '21:00', '22:00'];
   /* The page should display the existing booked table times & available slots
   using a list component containing several instances of a BookingSlot component.
   Available booking slots will be shared between the components and updated when the user submits the form */
@@ -13,11 +15,12 @@ export default function Main() {
   // map through array passed to it and generate a list of BookingSlot components with the array item as its child
   const AvailableSlots = times => times.map(i => <BookingSlot key={i}>{i}</BookingSlot>)
   // initialize availableTimes state as an empty array of time slots
-  const initializeTimes = AvailableSlots([])
+  const initializeTimes = () => AvailableSlots([])
   // reducer function for availableTimes state
   // certain available times under time input field will be displayed depending on what date is used in the form
   const updateTimes = (v, { type }) => {
-    // for now, make it so time options are populated when today's date or tomorrow's date is selected & time options are static
+    /* For now, make it so time options are populated when today's date or tomorrow's date is selected.
+    For now, time options are the same no matter what date you choose */
     const today = new Date() //get today's date & convert it to <input type='date'> input date format: yyyy-mm-dd
     const todayFormatted = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0')
       + '-' + String(today.getDate()).padStart(2, '0')
@@ -25,9 +28,9 @@ export default function Main() {
     const tomorrowFormatted = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0')
       + '-' + String(today.getDate() + 1).padStart(2, '0')
     if (type === todayFormatted)
-      return (['17:00','18:00', '19:00', '20:00', '21:00', '22:00'].map(i => <BookingSlot key={i}>{i}</BookingSlot>))
+      return (AvailableSlots(dummyTimes))
     if (type === tomorrowFormatted)
-      return (['17:00','18:00', '19:00', '20:00', '21:00', '22:00'].map(i => <BookingSlot key={i}>{i}</BookingSlot>))
+      return (AvailableSlots(dummyTimes))
     // if none of the actions selected, then don't update the state, hence what is returned for update is just the current state
     return v }
   // state lifted from BookingPage.js to be passed to BookingPage.js instead along with its dispatch function, state changed to a reducer
