af0b82d5376a56d62298eca3f5572899e2f85f48
edwardlosangeles <edward.n.delacruz@gmail.com>
2024-06-20 22:07:03 -0700

    created a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// src/App.test.js
+// ====================================================
+import { render, screen } from "@testing-library/react";
+import BookingPage from './pages/BookingPage';
+describe("booking form tests", () => {
+  test('Renders the BookingForm heading and Choose date label', () => {
+    render(<BookingPage />);
+    const headingElement = screen.getByText('Book Now');
+    const labelElement = screen.getByText('Choose date');
+    expect(headingElement).toBeInTheDocument();
+    expect(labelElement).toBeInTheDocument();
+  })
+})
