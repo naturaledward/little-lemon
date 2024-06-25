ea9c7932f12aa331bb17cdaf23914abfcc305ff9
edwardlosangeles <edward.n.delacruz@gmail.com>
2024-06-24 23:43:32 -0700

    added styling to confirm page

--- src/pages/ConfirmedBooking.js
+++ src/pages/ConfirmedBooking.js
@@ -1,12 +1,10 @@
 // src/pages/ConfirmedBooking.js
 // ====================================================
-import React from 'react'
-const ConfirmedBooking = ({ dateVal, timeVal, guests, occasion }) => {
-  return <>
-    <h1>Date: {dateVal}</h1>
-    <h1>Time: {timeVal}</h1>
-    <h1>Number of guests: {guests}</h1>
-    <h1>Occasion: {occasion}</h1>
-  </>
-}
+const ConfirmHero = ({ dateVal, timeVal, guests, occasion }) =>
+  <section className='bookingHero'> {/* retained layout and style of bookingHero */}
+    <div className='contentArea'>
+      <div><h1>Your reservation has been confirmed!</h1>
+        <p>We look forward to seeing you on {dateVal} at {timeVal} for your occasion: {occasion}! Number of guests: {guests}</p> </div>
+      <img src={require('../assets/logo_lemon_primary.png')} alt='hero reserve' /> </div> </section>
+const ConfirmedBooking = ({ d, t, g, o }) => <ConfirmHero dateVal={d} timeVal={t} guests={g} occasion={o} />
 export default ConfirmedBooking
